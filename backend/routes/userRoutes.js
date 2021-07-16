f392f9cf96e973c9d9b0235564a8e23a86f4a106
hirunagrad <hirunagrad@gmail.com>
2021-07-16 23:11:49 +0530

    front end connect mongoDB

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser } = require('../controllers/userControllers');
+const { registerUser, authUser } = require('../controllers/userControllers');
 
 
 
@@ -7,6 +7,7 @@ const router = express.Router()
 
 
 router.route("/").post(registerUser);
+router.route("/login").post(authUser);
 
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Header from './Header';
 import Form from './Form';
 import Home from './Home'
 import {BrowserRouter as Router,Route} from 'react-router-dom';
+import loggins from './loggins';
+import registers from './registers';
 
 
 
@@ -14,7 +16,10 @@ function App() {
       <br/> <br/> <br/>
     
     <Route path="/" component={Form} exact/>
+    <Route path="/login" component={loggins}/>
+    <Route path="/register" component={registers}/>
     <Route path="/home" component={Home}/>
+   
    
      
 
