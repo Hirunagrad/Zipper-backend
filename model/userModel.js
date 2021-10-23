9173cdae56ca8857cb1c61a5f0ac55cfb1744401
hirunagrad <hirunagrad@gmail.com>
2021-10-23 21:04:23 +0530

    update pic link

--- model/userModel.js
+++ model/userModel.js
@@ -24,8 +24,6 @@ const userSchema = mongoose.Schema(
     pic: {
       type: String,
       required: true,
-      default:
-        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
   },
   {
